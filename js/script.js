3f8bb8077aed57130a73d79ee9f489fb4580c6a9
lyf <lyf@yeastar.com>
2017-01-09 09:51:22 +0800

    update

--- js/script.js
+++ js/script.js
@@ -9,4 +9,17 @@ $(function(){
 		else
 			$('.blog-nav-inner').removeClass('is-sticky');
 	})
+	$('.popup-flower').click(function(){
+		var img=$('.bg-flower').css('background-image');
+		if(img!='none')
+		{
+			$('.bg-flower').css('background-image','none');
+			$(this).find('.fa').removeClass('fa-eye').addClass('fa-eye-slash');
+		}
+		else
+		{
+			$('.bg-flower').css('background-image',"url('/images/flower.png')");
+			$(this).find('.fa').removeClass('fa-eye-slash').addClass('fa-eye');
+		}
+	});
 })
